1ba6e05e6aef6166478c565024862702df352817
Dineshkumar0212 <165248237+Dineshkumar0212@users.noreply.github.com>
2024-06-09 19:05:38 +0530

    Update Bayesian Classifier.py

--- Bayesian Classifier.py
+++ Bayesian Classifier.py
@@ -29,7 +29,7 @@ class NaiveBayesClassifier:
         return posterior + np.log(prior)
 
 def main():
-    st.write("22AIA-MACHINE MASTERS")
+    st.write("BINARY WIZARDS")
     st.title("Tennis Data Classifier")
 
     # File upload
